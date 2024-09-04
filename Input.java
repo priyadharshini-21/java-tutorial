56cfb4c8658b83aa3fc651b0893e31e03cdaa940
priyadharshini-21 <priyadharshinilavanya21@gmail.com>
2024-09-04 18:47:00 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,15 @@
+public class Input {
+    public static void main(String a[])
+    {
+        String name="variablz-academy";
+        int year=6;
+        String address="hougang";
+        float reviews=4.6f;
+        System.out.println("name"+name);
+        System.out.println("year"+year);
+        System.out.println("address"+address);
+        System.out.println("reviews"+reviews);
+
+    }
+    
+}
