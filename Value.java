cc1f44ee0cb1adcd145ffa63efa49959e1333940
priyadharshini-21 <priyadharshinilavanya21@gmail.com>
2024-09-13 19:18:50 +0530

    Add files via upload

---
+++ Value.java
@@ -0,0 +1,9 @@
+public class Value {
+    public static void main(String a[]) {
+        var value=2*1+1-2*5;
+        System.out.println("Output:"+value);
+        value=2*((1+1)-2)*5;
+        System.out.println("Output:+value");
+     }
+    
+}
