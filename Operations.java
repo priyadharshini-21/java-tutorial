206d9417cf02f362afa1bb3c8b0310f42aba836a
priyadharshini-21 <priyadharshinilavanya21@gmail.com>
2024-09-13 19:18:03 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,11 @@
+public class Operations {
+    public static void main(String a[]) {
+        int x,y,z;
+        x=10;
+        y=30;
+        z=x+y;
+        System.out.println("\nAddition:"+z);
+    
+    }
+    
+}
